122ece2700d4b9b2d32264852cb34b5e950abbfc
Jamile <jamilelima@protonmail.com>
2020-10-01 16:55:27 -0300

    fix: adjust imports

--- app/src/main/java/dev/jamile/githubapp/models/Repository.kt
+++ app/src/main/java/dev/jamile/githubapp/models/Repository.kt
@@ -1,5 +1,4 @@
-package com.example.githubapp.models
-
+import com.example.githubapp.models.Owner
 import com.google.gson.annotations.SerializedName
 
 data class Repository(
@@ -15,5 +14,4 @@ data class Repository(
     val startGazersCount: Int,
     val watchers: Int,
     val language: String,
-
 )
--- app/src/main/java/dev/jamile/githubapp/models/SearchResponse.kt
+++ app/src/main/java/dev/jamile/githubapp/models/SearchResponse.kt
@@ -1,4 +1,3 @@
-package com.example.githubapp.models
 
 import com.google.gson.annotations.SerializedName
 
