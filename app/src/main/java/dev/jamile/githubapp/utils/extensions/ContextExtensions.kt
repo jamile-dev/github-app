a387cd3d4b8c01c145696bae42976c4ba8d7761c
Jamile <jamilelima@protonmail.com>
2020-10-02 22:25:44 -0300

    feat: create extension

--- app/src/main/java/dev/jamile/githubapp/utils/extensions/ContextExtensions.kt
+++ app/src/main/java/dev/jamile/githubapp/utils/extensions/ContextExtensions.kt
@@ -1,10 +1,17 @@
 package dev.jamile.githubapp.utils.extensions
 
 import android.content.Context
+import android.net.ConnectivityManager
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Context.hideKeyboard(view: View?) {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view?.applicationWindowToken, 0)
+}
+
+fun Context.haveConnection(): Boolean {
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return connectivityManager.activeNetwork != null
 }
