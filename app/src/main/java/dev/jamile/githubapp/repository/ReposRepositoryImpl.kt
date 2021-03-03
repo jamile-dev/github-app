f8428f0dddceea915a1d1b2421ebe392232efc09
Jamile <jamilelima@protonmail.com>
2021-03-02 21:48:16 -0300

    chore: improve nullability

--- app/src/main/java/dev/jamile/githubapp/repository/ReposRepositoryImpl.kt
+++ app/src/main/java/dev/jamile/githubapp/repository/ReposRepositoryImpl.kt
@@ -9,16 +9,20 @@ class ReposRepositoryImpl(private val apiService: ApiService) : ReposRepository 
     override suspend fun getRepositories(): Result<SearchResponse> {
         val response = apiService.fetchPopularRepos()
         if (response.isSuccessful && response.body()?.incompleteResults == false) {
-            return Result.Success(response.body()!!)
+            response.body()?.let { popularReposResponse ->
+                return Result.Success(popularReposResponse)
+            }
         }
-        return Result.Failure(Throwable("Error ${response.errorBody()} ${response.message()} "))
+        return Result.Failure(Throwable("Error ${response.errorBody()} ${response.message()}"))
     }
 
     override suspend fun searchRepositories(query: String): Result<SearchResponse> {
         val response = apiService.searchRepositories(query)
         if (response.isSuccessful) {
-            return Result.Success(response.body()!!)
+            response.body()?.let { searchResponse ->
+                return Result.Success(searchResponse)
+            }
         }
-        return Result.Failure(Throwable("Error ${response.errorBody()} ${response.message()} "))
+        return Result.Failure(Throwable("Error ${response.errorBody()} ${response.message()}"))
     }
 }
